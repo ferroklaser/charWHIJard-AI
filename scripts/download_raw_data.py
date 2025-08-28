067b4d87ec52eee6cefaa31314dc0288408fa322
jaeden <jaedenthia22@gmail.com>
2025-08-28 15:10:16 +0800

    feat: data download script implemented

---
+++ scripts/download_raw_data.py
@@ -0,0 +1,29 @@
+import os
+import requests
+
+# Pls follow this format for adding datasets -> 'filename': 'download_url'
+# This is meant for downloading raw data sets so we can process them using preprocessing scripts
+# Make sure the google drive csv file is set to "anyone w the link can edit the file"
+datasets = {
+    # i.e. "south_dakota.csv": "https://drive.google.com/file/d/175FN8ZY-mnJySrWvN11vLFsRAn1d4rEv/view?usp=sharing",
+    # Add other datasets here
+}
+
+os.makedirs("../data/raw", exist_ok=True)
+
+for filename, url in datasets.items():
+    output_path = os.path.join("../data/raw", filename)
+    if os.path.exists(output_path):
+        print(f"{filename} already exists, skipping download.")
+        continue
+
+    print(f"downloading {filename}")
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        with open(output_path, "wb") as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+        print(f"{filename} downloaded")
+    except Exception as e:
+        print(f"Failed to download {filename}: {e}")
